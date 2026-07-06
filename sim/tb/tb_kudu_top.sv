c7570e18313a9eb9e762ffeb8410573ce1a0d816
kunyanliu <kunyanliu@microsoft.com>
2026-07-05 17:27:21 -0700

    removed unused force statements for dii sim setup

--- sim/tb/tb_kudu_top.sv
+++ sim/tb/tb_kudu_top.sv
@@ -205,9 +205,9 @@ module tb_kudu_top;
 
       force dut.regfile_i.rf_reg_q = {15{TM_ROOT_RCAP}};
             
-      force dut.cs_registers_i.mepc_q[31:0] = 32'h8000_0000;
+      // force dut.cs_registers_i.mepc_q[31:0] = 32'h8000_0000;
 
-      force dut.cs_registers_i.init_mtvec32 = 32'h0000_0000;
+      // force dut.cs_registers_i.init_mtvec32 = 32'h0000_0000;
       // cheriot-sail configuration doesn't support PRIV_LVL_U
       force dut.cs_registers_i.mstatus_q[3:2] = 2'b11;
 
